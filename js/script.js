7c597eaef545f5cf309fc42be67e0796b94115f7
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2023-08-20 00:36:39 +0600

    code ready

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ const makePurchase = document.getElementById("makeIt");
 
 // initial total
 let totalPrice = 0;
+let voucherActive = false;
+
+// if voucher added
+function orderCalculate(){
+    const discountTotal = (totalPrice * 20) / 100;
+    const subTotal = totalPrice - discountTotal;
+    discount.innerText = discountTotal.toFixed(2);
+    orderTotal.innerText = subTotal.toFixed(2);
+}
 
 // product adding to cart
 function itemAdder(itemNameId, itemPriceId) {
@@ -32,16 +41,18 @@ function itemAdder(itemNameId, itemPriceId) {
             apply.removeAttribute("disabled", false);
         }
     }
+    // if voucher activated
+    if (voucherActive) {
+        orderCalculate();
+    }
 }
 
 // voucher apply button
 apply.addEventListener("click", function () {
     const discountInput = discountInputText.value;
     if (discountInput === 'SELL200') {
-        const discountTotal = (totalPrice * 20) / 100;
-        const subTotal = totalPrice - discountTotal;
-        discount.innerText = discountTotal.toFixed(2);
-        orderTotal.innerText = subTotal.toFixed(2);
+        orderCalculate();
+        voucherActive = true;
     } else {
         alert("Invalid Voucher!");
     }
@@ -57,4 +68,5 @@ function goHome() {
     discount.innerText = '00.00';
     apply.setAttribute("disabled", true);
     makePurchase.setAttribute("disabled", true);
+    voucherActive = false;
 }
